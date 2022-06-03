2bc5ee0f1adb417002fa91c1a8e605be7f0fd8c2
Fernando Suarez <fsuarez1@uc.cl>
2022-06-03 14:14:58 -0400

    change setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fintualistic',
-    version='0.0',
+    version='0.1',
     author="Fernando Suarez",
     author_email='fsuarez1@uc.cl',
     packages=find_packages('src'),
