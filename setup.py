60bcbee973897b4aae7ebad2c743b0e9144dabd1
Fernando Suarez <fsuarez1@uc.cl>
2022-06-03 15:41:04 -0400

    problems with versions

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.rst").read_text()
 
 setup(
     name='fintualistic',
-    version='0.3.2',
+    version='0.3.3',
     author="Fernando Suarez",
     author_email='fsuarez1@uc.cl',
     packages=find_packages('src'),
