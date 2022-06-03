1901729d30cb6345cf9694e8e8b8c07aac6c2dba
Fernando Suarez <fsuarez1@uc.cl>
2022-06-03 13:58:08 -0400

    change url

--- setup.py
+++ setup.py
@@ -4,12 +4,11 @@ from setuptools import setup, find_packages
 setup(
     name='fintualistic',
     version='0.0',
-    license='Fintual',
     author="Fernando Suarez",
     author_email='fernando@fintual.com',
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    url='https://github.com/gmyrianthous/example-publish-pypi',
+    url='https://github.com/fsuarezb/fintualistic',
     keywords='fintual fintualistic',
     install_requires=[
           'pandas', 'plotly'
