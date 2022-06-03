f54b63db2fe34bc5da5f97ac9dd60039fa42cb69
Fernando Suarez <fsuarez1@uc.cl>
2022-06-03 14:20:28 -0400

    add description

--- setup.py
+++ setup.py
@@ -1,5 +1,15 @@
 from setuptools import setup, find_packages
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.rst").read_text()
 
+setup(
+    name='an_example_package',
+    # other arguments omitted
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
 
 setup(
     name='fintualistic',
