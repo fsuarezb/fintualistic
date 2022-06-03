64b027c3c14c8bcbae62816a8cedcdd90ec87b2b
Fernando Suarez <fsuarez1@uc.cl>
2022-06-03 14:05:05 -0400

    change mail

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='fintualistic',
     version='0.0',
     author="Fernando Suarez",
-    author_email='fernando@fintual.com',
+    author_email='fsuarez1@uc.cl',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     url='https://github.com/fsuarezb/fintualistic',
